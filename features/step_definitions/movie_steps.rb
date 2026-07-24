76a847c5f9dd6697cf659bdd15cd8745b512f555
Monica Chen <monicachen@berkeley.edu>
2026-07-24 22:15:24 +0000

    chip 7.7

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given(/the following movies exist/) do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  pending "Fill in this step in movie_steps.rb"
 end
 
 Then(/(.*) seed movies should exist/) do |n_seeds|
@@ -18,7 +18,9 @@ end
 Then(/^I should see "(.*)" before "(.*)" in the movie list$/) do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  pending "Fill in this step in movie_steps.rb"
+  pos1 = page.body.index(e1)
+  pos2 = page.body.index(e2)
+  expect(pos1).to be < pos2
 end
 
 
@@ -29,17 +31,31 @@ When(/I check the following ratings: (.*)/) do |rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  ratings = rating_list.split(",")
+  ratings.each do |rating|
+    rating = rating.strip
+    step %Q{I check "#{rating}"}
+  end
 end
 
 Then(/^I should (not )?see the following movies: (.*)$/) do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  movies = movie_list.split(",")
+  movies.each do |movie|
+    movie = movie.strip
+    if no
+      step %Q{I should not see "#{movie}"}
+    else
+      step %Q{I should see "#{movie}"}
+    end
+  end
 end
 
 Then(/^I should see all the movies$/) do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  Movie.all.each do |movie|
+    step %Q{I should see "#{movie.title}"}
+  end
 end
 
 ### Utility Steps Just for this assignment.
